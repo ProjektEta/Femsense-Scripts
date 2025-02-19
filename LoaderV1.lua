f24f997ec2dd35b9fc1f14383571ae92b7e01af8
Eta <pleasefckoff00@gmail.com>
2025-02-19 20:22:11 +0000

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -1,6 +1,6 @@
 
 local places = {
-    [16732694052] = "https://raw.githubusercontent.com/ProjektEta/Femscr.dll/refs/heads/main/Scripts/Fisch/Premium.lua",
+    [16732694052] = "https://raw.githubusercontent.com/ProjektEta/Femsense-Scripts/refs/heads/main/V1/Fisch/Main.lua",
     [18901165922] = 'https://raw.githubusercontent.com/ProjektEta/Femscr.dll/refs/heads/main/Scripts/Pets%20Go/Premium.lua'
 }
 
